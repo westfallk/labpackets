57c010d31e4c3d0f0b8f4ebb472f30abea48e501
Kole Westfall <westfallk@sou.edu>
2022-02-02 15:14:24 -0800

    added functionality to have different user input

--- chargen.py
+++ chargen.py
@@ -1,4 +1,17 @@
 import sys
 from scapy.all import *
-dstIP = input()
-send(IP(dst=dstIP)/UDP(dport=19,sport=19)/Raw(load="who is out there?"))
+def sendPacketWithSrc(dstIP, srcIP):
+    send(IP(dst=dstIP, src=srcIP)/UDP(dport=19,sport=19)/Raw(load="who is out there?"))
+def sendPacket(dstIP):
+    send(IP(dst=dstIP)/UDP(dport=19,sport=19)/Raw(load="who is out there?"))
+
+
+userIn = input()
+ipArr = userIn.split(' ')
+if(len(ipArr)>2):
+    print("pass in two IPs in dotted decimal notation seperated by a space")
+elif(len(ipArr)==2):
+    sendPacketWithSrc(ipArr[0],ipArr[1])
+else:
+    sendPacket(ipArr[0])
+
