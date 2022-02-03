149b5da39db659bfce781540168e256b042d9ebb
Kole Westfall <westfallk@sou.edu>
2022-02-02 16:18:48 -0800

    added ability to wait before exiting

--- disableFirewall.py
+++ disableFirewall.py
@@ -1,4 +1,5 @@
 import telnetlib
+import time
 HOST = "192.168.96.1"
 #HOST = input()
 
@@ -20,5 +21,5 @@ tn.write(b"config t\n")
 
 tn.read_until(b"RouterA#(config)")
 tn.write(b"banner motd #This was automated#\n")
-
+time.sleep(2)
 tn.write(b"exit\n")
